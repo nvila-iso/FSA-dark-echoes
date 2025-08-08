b43e2cbcc8b99e97679aa1f6b5cdc922de271dea
nvila-iso <nvila88@proton.me>
2025-08-08 15:55:49 -0400

    updated js / css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
+import { episodeList } from "./data"; // import episode list data
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList); // store into a variable
+  const [selectedEpisode, setSelectedEpisode] = useState(); // keeps the selected episode in a state variable
+
+  /** Detailed information on Episode */
+  const EpisodeDetails = () => {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <img src="./imgs/darkechoes-logo.png"></img>
+          <p>Select an episode for details.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>- {selectedEpisode.title} -</h2>
+        <img src={selectedEpisode.img} alt={selectedEpisode.title}></img>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  };
+
+  // list all episodes
+  const ListEpisodes = () => {
+    return (
+      <section className="list">
+        <h2>List of Episodes</h2>
+        <ul className="list">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  };
+  return (
+    <>
+      <header>
+        <a href="index.html">
+          <h1>DARK ECHOES</h1>
+        </a>
+      </header>
+      <main>
+        <ListEpisodes></ListEpisodes>
+        <EpisodeDetails></EpisodeDetails>
+      </main>
+    </>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -4,47 +4,55 @@ export const episodeList = [
     title: "The Silent Caller",
     description:
       "A mysterious phone call leads a detective into a dark conspiracy.",
+    img: "./imgs/the_silent_caller.png",
   },
   {
     id: 2,
     title: "Whispers in the Night",
     description:
       "Strange noises in a small town raise suspicion about the disappearance of a local.",
+    img: "./imgs/whispers_in_the_night.png",
   },
   {
     id: 3,
     title: "Into the Abyss",
     description:
       "A deep dive into a secretive company unearths buried secrets.",
+    img: "./imgs/into_the_abyss.png",
   },
   {
     id: 4,
     title: "The Vanishing Point",
     description:
       "A witness vanishes before she can testify, throwing the case into chaos.",
+    img: "./imgs/the_vanishing_point.png",
   },
   {
     id: 5,
     title: "Shadows of the Past",
     description:
       "A detective’s past resurfaces as a familiar face becomes a prime suspect.",
+    img: "./imgs/shadows_of_the_past.png",
   },
   {
     id: 6,
     title: "The Last Betrayal",
     description:
       "An unexpected betrayal puts everyone’s lives in danger as the real villain is revealed.",
+    img: "./imgs/the_last_betrayal.png",
   },
   {
     id: 7,
     title: "Echoes of Fear",
     description:
       "A chilling pattern of murders that echo an old crime spree leads to a shocking discovery.",
+    img: "./imgs/echoes_of_fear.png",
   },
   {
     id: 8,
     title: "The Final Hour",
     description:
       "In the thrilling season finale, every truth comes to light, but the consequences are deadly.",
+    img: "./imgs/the_final_hour.png",
   },
 ];
